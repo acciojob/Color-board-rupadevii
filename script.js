ba4b57b855e61221c35f3dcde0dd9db5599f7ac1
Rupa Devi <rupadevi7094@gmail.com>
2025-11-09 20:14:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 //your JS code here. If required.
+const container = document.querySelector('.container');
+const colors = ["red", "black", "blue", "green", "yellow", "orange", "pink", "purple", "orange","white"]
+
+for(let i=0; i<800; i++){
+	const div = document.createElement('div')
+	div.className = 'square';
+	container.append(div)
+	div.addEventListener("mouseover", () =>{
+		div.style.backgroundColor = colors[Math.floor(Math.random() * 10)]
+	})
+	div.addEventListener("mouseout", () => {
+		div.style.backgroundColor = "gray";
+		div.classList.add("reset")
+	})
+}
